cff3aee0102693e6f4741108158c00ed0577f4e5
Leonardo Nogueira <leonardo.ngra@gmail.com>
2022-10-29 10:18:34 -0300

    feat: Adicionando loading na listagem de pessoas

--- src/pages/listagem-de-pessoas/index.tsx
+++ src/pages/listagem-de-pessoas/index.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useMemo, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { LinearProgress, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from "@mui/material";
 
 import { LayoutBaseDePagina } from "../../shared/layouts/LayoutBaseDePagina";
 import { IListagemPessoa, PessoasService } from "../../shared/services/api/PessoasService";
 import { FerramentasDaListagem } from "../../shared/components/ferramentas-da-listagem";
 import { useDebounce } from "../../shared/hooks/useDebounce";
+import { Environment } from "../../shared/environment";
 
 export const ListagemDePessoas = () => {
     const [searchParams, setSearchParams] = useSearchParams();
@@ -50,8 +51,8 @@ export const ListagemDePessoas = () => {
                     textoDaBusca={busca}
                     aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
                 />}>
-            
-            <TableContainer component={Paper} variant='outlined' sx={{ m: 1, width: 'auto'}}>
+
+            <TableContainer component={Paper} variant='outlined' sx={{ m: 1, width: 'auto' }}>
                 <Table>
                     <TableHead>
                         <TableRow>
@@ -62,14 +63,28 @@ export const ListagemDePessoas = () => {
                     </TableHead>
 
                     <TableBody>
-                    {rows.map(row => (
-                        <TableRow key={row.id}>
-                        <TableCell>Ações</TableCell>
-                        <TableCell>{row.nomeCompleto}</TableCell>
-                        <TableCell>{row.email}</TableCell>
-                    </TableRow>
-                    ))}
+                        {rows.map(row => (
+                            <TableRow key={row.id}>
+                                <TableCell>Ações</TableCell>
+                                <TableCell>{row.nomeCompleto}</TableCell>
+                                <TableCell>{row.email}</TableCell>
+                            </TableRow>
+                        ))}
                     </TableBody>
+
+                    {totalCount === 0 && !isLoading && (
+                        <caption>{Environment.LISTAGEM_VAZIA}</caption>
+                    )}
+
+                    <TableFooter>
+                        {isLoading && (
+                            <TableRow>
+                                <TableCell colSpan={3}>
+                                    <LinearProgress variant='indeterminate' />
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableFooter>
                 </Table>
             </TableContainer>
 
