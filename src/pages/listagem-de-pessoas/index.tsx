387b83ab849c0496216004949c13c63dd4ec5500
Leonardo Nogueira <leonardo.ngra@gmail.com>
2022-10-29 10:50:34 -0300

    feat:  Adicionando paginação na listagem

--- src/pages/listagem-de-pessoas/index.tsx
+++ src/pages/listagem-de-pessoas/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import { LinearProgress, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from "@mui/material";
+import { LinearProgress, Pagination, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from "@mui/material";
 
 import { LayoutBaseDePagina } from "../../shared/layouts/LayoutBaseDePagina";
 import { IListagemPessoa, PessoasService } from "../../shared/services/api/PessoasService";
@@ -11,84 +11,99 @@ import { Environment } from "../../shared/environment";
 export const ListagemDePessoas = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     const { debounce } = useDebounce();
-
+  
     const [rows, setRows] = useState<IListagemPessoa[]>([]);
-    const [totalCount, setTotalCount] = useState(0);
     const [isLoading, setIsLoading] = useState(true);
-
+    const [totalCount, setTotalCount] = useState(0);
+  
+  
     const busca = useMemo(() => {
-        return searchParams.get('busca') || '';
+      return searchParams.get('busca') || '';
     }, [searchParams]);
-
+  
+    const pagina = useMemo(() => {
+      return Number(searchParams.get('pagina') || '1');
+    }, [searchParams]);
+  
+  
     useEffect(() => {
-        setIsLoading(true)
-
-        debounce(() => {
-            PessoasService.getAll(1, busca)
-                .then((result) => {
-                    setIsLoading(false)
-
-                    if (result instanceof Error) {
-                        alert(result.message)
-                    } else {
-                        console.log(result)
-
-                        setRows(result.data)
-                        setTotalCount(result.totalCount)
-                    }
-                })
-        })
-    }, [busca]);
-
+      setIsLoading(true);
+  
+      debounce(() => {
+        PessoasService.getAll(pagina, busca)
+          .then((result) => {
+            setIsLoading(false);
+  
+            if (result instanceof Error) {
+              alert(result.message);
+            } else {
+              console.log(result);
+  
+              setTotalCount(result.totalCount);
+              setRows(result.data);
+            }
+          });
+      });
+    }, [busca, pagina]);
+  
+  
     return (
-
-        <LayoutBaseDePagina
-            titulo='Listagem de pessoas'
-            barraDeFerramentas={
-                <FerramentasDaListagem
-                    mostrarInputBusca
-                    textoBotaoNovo="Nova"
-                    textoDaBusca={busca}
-                    aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
-                />}>
-
-            <TableContainer component={Paper} variant='outlined' sx={{ m: 1, width: 'auto' }}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Ações</TableCell>
-                            <TableCell>Nome completo</TableCell>
-                            <TableCell>Email</TableCell>
-                        </TableRow>
-                    </TableHead>
-
-                    <TableBody>
-                        {rows.map(row => (
-                            <TableRow key={row.id}>
-                                <TableCell>Ações</TableCell>
-                                <TableCell>{row.nomeCompleto}</TableCell>
-                                <TableCell>{row.email}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-
-                    {totalCount === 0 && !isLoading && (
-                        <caption>{Environment.LISTAGEM_VAZIA}</caption>
-                    )}
-
-                    <TableFooter>
-                        {isLoading && (
-                            <TableRow>
-                                <TableCell colSpan={3}>
-                                    <LinearProgress variant='indeterminate' />
-                                </TableCell>
-                            </TableRow>
-                        )}
-                    </TableFooter>
-                </Table>
-            </TableContainer>
-
-        </LayoutBaseDePagina>
-
-    )
-};
+      <LayoutBaseDePagina
+        titulo='Listagem de pessoas'
+        barraDeFerramentas={
+          <FerramentasDaListagem
+            mostrarInputBusca
+            textoDaBusca={busca}
+            textoBotaoNovo='Nova'
+            aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto, pagina: '1' }, { replace: true })}
+          />
+        }
+      >
+        <TableContainer component={Paper} variant="outlined" sx={{ m: 1, width: 'auto' }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Ações</TableCell>
+                <TableCell>Nome completo</TableCell>
+                <TableCell>Email</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map(row => (
+                <TableRow key={row.id}>
+                  <TableCell>Ações</TableCell>
+                  <TableCell>{row.nomeCompleto}</TableCell>
+                  <TableCell>{row.email}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+  
+            {totalCount === 0 && !isLoading && (
+              <caption>{Environment.LISTAGEM_VAZIA}</caption>
+            )}
+  
+            <TableFooter>
+              {isLoading && (
+                <TableRow>
+                  <TableCell colSpan={3}>
+                    <LinearProgress variant='indeterminate' />
+                  </TableCell>
+                </TableRow>
+              )}
+              {(totalCount > 0 && totalCount > Environment.LIMITE_DE_LINHAS) && (
+                <TableRow>
+                  <TableCell colSpan={3}>
+                    <Pagination
+                      page={pagina}
+                      count={Math.ceil(totalCount / Environment.LIMITE_DE_LINHAS)}
+                      onChange={(_, newPage) => setSearchParams({ busca, pagina: newPage.toString() }, { replace: true })}
+                    />
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableFooter>
+          </Table>
+        </TableContainer>
+      </LayoutBaseDePagina>
+    );
+  };
