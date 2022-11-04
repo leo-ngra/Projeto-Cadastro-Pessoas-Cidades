6907e348637d5287e7cc2d341a13e9dc5beb75bb
Leonardo Nogueira <leonardo.ngra@gmail.com>
2022-11-04 09:07:25 -0300

    feat: Inserindo tradução no Yup

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 
+import { BrowserRouter } from "react-router-dom";
+
+import './shared/form/TraducoesYup';
+
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import { BrowserRouter } from "react-router-dom";
 import { AppThemeProvider } from './shared/contexts/ThemeContext';
 import { DrawerProvider } from './shared/contexts/DrawerContext';
 
